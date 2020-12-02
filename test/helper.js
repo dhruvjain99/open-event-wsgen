f5e46e2ae18b3334d17b4e7c5c147ffd936c1c9d
Dhruv Jain <dhruv.jain93@gmail.com>
2020-12-02 19:47:19 +0530

    dfg

--- test/helper.js
+++ test/helper.js
@@ -11,8 +11,9 @@ function getDriver() {
         }
       },
       args: [
-        '--window-size=1920,1080',
+        'window-size=1920x1080',
         '--start-maximized',
+        '--headless'
       ]
     }
   };
