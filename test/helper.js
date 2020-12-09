cb1ad614e766e4240e2bf5a5160c9ce544de0aa6
Dhruv Jain <dhruv.jain93@gmail.com>
2020-12-09 13:25:57 +0530

    set window size to 1600x1200

--- test/helper.js
+++ test/helper.js
@@ -11,9 +11,8 @@ function getDriver() {
         }
       },
       args: [
-        '--window-size=1920,1080',
+        '--window-size=1600,1200',
         '--start-maximized',
-        '--no-sandbox'
       ],
       localState: {
         "browser.enabled_labs_experiments": ["calculate-native-win-occlusion@2"],
